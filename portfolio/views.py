018015bc738294f6712ec7375d3308529f6d685c
Pedro Lopez <pedro.lopez.isw@gmail.com>
2021-02-22 20:50:08 -0600

    first commit for home header, adding some animations with animejs

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def contact(request):
     return render(request, 'contact.html')
