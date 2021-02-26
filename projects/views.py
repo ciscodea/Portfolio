1df4b8ec440a1bf7159be5e72ec8c9f9fc4f062e
Pedro Lopez <pedro.lopez.isw@gmail.com>
2021-02-25 18:38:08 -0600

    resolving a overlaping footer to content

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+
+# Models
+from projects.models import Project
 # Create your views here.
 
 
 def show_projects(request):
-    return render(request, 'projects/feed.html')
+    project = Project.objects.all()
+
+    return render(request, 'projects/feed.html', {'projects' : project})
