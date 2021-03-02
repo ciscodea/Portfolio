ea0f1700d4ada952ae46c99f9c00c4705f5f06d0
Pedro Lopez <pedro.lopez.isw@gmail.com>
2021-03-02 13:05:31 -0600

    Configurations

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'vex(y)&b^gn$8jt@^!=sxj$o%mhbib=jj+wji^e1p=cvp)6%q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
