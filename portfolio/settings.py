e02a249aeba34a76886bdd4b1da83d74c118eb03
Pedro Lopez <pedro.lopez.isw@gmail.com>
2021-03-21 11:16:28 -0600

    Add configurations for development purposes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'vex(y)&b^gn$8jt@^!=sxj$o%mhbib=jj+wji^e1p=cvp)6%q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -51,9 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    #for deployment purposes
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -79,21 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-"""
-
-import dj_database_url
-from decouple import config
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
 }
 
 # Password validation
@@ -147,31 +135,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 LOGIN_URL = '/users/login/'
 
-#For deployment purposes
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-
 # Clodinary for storage images
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-"""
 cloudinary.config( 
   cloud_name = "he9xujseo", 
   api_key = "254211853684524", 
   api_secret = "SrF6WiXygiXs3UpkHH-hk8iIFAc" 
 )
-"""
 
-#For deployment purposes
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'he9xujseo',
-    'API_KEY': '254211853684524',
-    'API_SECRET': 'SrF6WiXygiXs3UpkHH-hk8iIFAc',
-}
 
 # config/settings.py
 DEFAULT_FROM_EMAIL = 'pedro.social.isw@gmail.com'
